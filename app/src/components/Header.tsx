375fc78b863133d74386473b96450f32dd1db5e6
Rahul Garud <84452921+Rahul123223@users.noreply.github.com>
2025-08-15 22:12:15 +0530

    Child branch (#10)
    * resolved issues
    
    * ui changes for radio buttons
    
    * resolved issues
    
    * resolved issues
    
    * resolved ui issues
    
    * ressolved issue
    
    * resolved issue
    
    * Chandrakant newchanges (#6)
    
    * Width change
    
    * customize routes & add common & product details button icon added
    
    * resolved issue
    
    ---------
    
    Co-authored-by: Rahul Garud <rahulgarud@Rahuls-MacBook-Air.local>
    Co-authored-by: chandrakant Deore <87793194+chandudeore@users.noreply.github.com>

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -61,7 +61,7 @@ const Header: React.FC<{ onSearch: (query: string) => void }> = ({
           }}
           onClick={() => (window.location.href = "/")}
         >
-          Shopee
+          RG Shop
         </Typography>
 
         {/* Search Bar */}
