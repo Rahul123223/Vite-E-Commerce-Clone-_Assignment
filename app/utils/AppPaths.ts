7ade8d59596503eb1bdfa61e9b815ae171a37a79
chandrakant Deore <87793194+chandudeore@users.noreply.github.com>
2025-08-15 20:19:44 +0530

    Chandrakant newchanges (#6)
    * Width change
    
    * customize routes & add common & product details button icon added

---
+++ app/utils/AppPaths.ts
@@ -0,0 +1,10 @@
+const APPPATHS = {
+  DASHBOARD: "/",
+  PRODUCTS: "/products",
+  LOGIN: "/login",
+  CART: "/cart",
+  ORDERS: "/orders",
+  PRODUCTDETAILS: `/product/:id`,
+};
+
+export { APPPATHS };
