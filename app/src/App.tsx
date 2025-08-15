1a3aabaa22cf5ca937970856c6e7285eff9301e0
Rahul Garud <rahulgarud@Rahuls-MacBook-Air.local>
2025-08-15 21:45:33 +0530

    resolved issue

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
@@ -12,83 +7,23 @@ import CartManager from "./pages/CartManager";
 import OrderManager from "./pages/OrderManager";
 import { CartProvider } from "./Context/CartContext";
 import { AuthProvider, useAuth } from "./Context/AuthContext";
-import Login from "./pages/Login";
-import { APPPATHS } from "../utils/AppPaths";
-
-interface RoutesArray {
-  path: string;
-  element: JSX.Element;
-}
+import Login from "./pages/Login"; 
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
-  const routesArray: RoutesArray[] = [
-    {
-      path: APPPATHS.DASHBOARD,
-      element: <Dashboard searchQuery={searchQuery} />,
-    },
-    { path: APPPATHS.LOGIN, element: <Login /> },
-    {
-      path: APPPATHS.PRODUCTDETAILS,
-      element: (
-        <ProtectedRoute>
-          <ProductDetails />
-        </ProtectedRoute>
-      ),
-    },
-    {
-      path: APPPATHS.CART,
-      element: (
-        <ProtectedRoute>
-          <CartManager />
-        </ProtectedRoute>
-      ),
-    },
-    {
-      path: APPPATHS.ORDERS,
-      element: (
-        <ProtectedRoute>
-          <OrderManager />
-        </ProtectedRoute>
-      ),
-    },
-  ];
 
   return (
     <AuthProvider>
       <CartProvider>
         <Router>
           <Header onSearch={(query) => setSearchQuery(query)} />
+
           <Routes>
-            {routesArray.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-            ))}
-            {/* <Route path="/" element={<Dashboard searchQuery={searchQuery} />} />
+            <Route path="/" element={<Dashboard searchQuery={searchQuery} />} />
             <Route path="/login" element={<Login />} />
-            <Route
-              path="/product/:id"
-              element={
-                <ProtectedRoute>
-                  <ProductDetails />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/cart"
-              element={
-                <ProtectedRoute>
-                  <CartManager />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/orders"
-              element={
-                <ProtectedRoute>
-                  <OrderManager />
-                </ProtectedRoute>
-              }
-            /> */}
+            <Route path="/product/:id" element={<ProtectedRoute><ProductDetails /></ProtectedRoute>} />
+            <Route path="/cart" element={<ProtectedRoute><CartManager /></ProtectedRoute>} />
+            <Route path="/orders" element={<ProtectedRoute><OrderManager /></ProtectedRoute>} />
           </Routes>
         </Router>
       </CartProvider>
