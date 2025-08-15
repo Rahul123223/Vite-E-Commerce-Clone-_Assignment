1f03e63a76f65a8f0ecf9a68fdd014dd7f2cffde
chandudeore <chandudeore2016@gmail.com>
2025-08-15 20:08:24 +0530

    customize routes & add common & product details button icon added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
@@ -7,23 +12,83 @@ import CartManager from "./pages/CartManager";
 import OrderManager from "./pages/OrderManager";
 import { CartProvider } from "./Context/CartContext";
 import { AuthProvider, useAuth } from "./Context/AuthContext";
-import Login from "./pages/Login"; 
+import Login from "./pages/Login";
+import { APPPATHS } from "../utils/AppPaths";
+
+interface RoutesArray {
+  path: string;
+  element: JSX.Element;
+}
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
+  const routesArray: RoutesArray[] = [
+    {
+      path: APPPATHS.DASHBOARD,
+      element: <Dashboard searchQuery={searchQuery} />,
+    },
+    { path: APPPATHS.LOGIN, element: <Login /> },
+    {
+      path: APPPATHS.PRODUCTDETAILS,
+      element: (
+        <ProtectedRoute>
+          <ProductDetails />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: APPPATHS.CART,
+      element: (
+        <ProtectedRoute>
+          <CartManager />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: APPPATHS.ORDERS,
+      element: (
+        <ProtectedRoute>
+          <OrderManager />
+        </ProtectedRoute>
+      ),
+    },
+  ];
 
   return (
     <AuthProvider>
       <CartProvider>
         <Router>
           <Header onSearch={(query) => setSearchQuery(query)} />
-
           <Routes>
-            <Route path="/" element={<Dashboard searchQuery={searchQuery} />} />
+            {routesArray.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+            {/* <Route path="/" element={<Dashboard searchQuery={searchQuery} />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/product/:id" element={<ProtectedRoute><ProductDetails /></ProtectedRoute>} />
-            <Route path="/cart" element={<ProtectedRoute><CartManager /></ProtectedRoute>} />
-            <Route path="/orders" element={<ProtectedRoute><OrderManager /></ProtectedRoute>} />
+            <Route
+              path="/product/:id"
+              element={
+                <ProtectedRoute>
+                  <ProductDetails />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/cart"
+              element={
+                <ProtectedRoute>
+                  <CartManager />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/orders"
+              element={
+                <ProtectedRoute>
+                  <OrderManager />
+                </ProtectedRoute>
+              }
+            /> */}
           </Routes>
         </Router>
       </CartProvider>
